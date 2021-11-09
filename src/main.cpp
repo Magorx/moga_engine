1969d7d6bee188340faa643f0bd5a1a45c6851a2
KingCakeTheFruity <maxim.gorishniy@gmail.com>
2021-11-09 23:29:55 +0300

    line seems to be done

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
+// #define TEST
+
+#ifndef TEST
 #include "redactor/engine.h"
 
 #include "app_initialization.h"
+#endif
 
+#include "utils/string_input/string_field.h"
 
 int main() {
+    #ifndef TEST
     srand(time(nullptr));
+
     RedactorEngine moga("MOGA", SCR_W, SCR_H, 1);
 
     Resources.init(&moga);
@@ -12,6 +19,39 @@ int main() {
     initialize_photoshop(moga);
 
     moga.everlasting_loop();
+    #else
+
+    #define D l.dump();
+
+    Line l(2); D
+
+    l.insert_char('a'); D
+    l.insert_char('b'); D
+    l.insert_char('_'); D
+    l.insert_char('c'); D
+    l.insert_char('d'); D
+
+    l.insert_char('1'); D
+    l.insert_char('2'); D
+    l.insert_char('3'); D
+    l.insert_char('4'); D
+
+    l.cursor_l(); D
+    l.cursor_l(); D
+    l.cursor_l(); D
+    l.cursor_l(); D
+    l.cursor_l(); D
+    l.cursor_l(); D
+
+
+    // l.fix_anchors(); D
+    // l.cursor_l(); D
+    // l.cursor_l(); D
+    // l.cursor_l(); D
+    l.erase_next(); D
+
+    printf("---\n[%s]\n", l.c_str());
+    #endif
 
 	return 0;
 }
--- src/utils/string_input/string_field.h
+++ src/utils/string_input/string_field.h
@@ -4,9 +4,49 @@
 #include <vector>
 #include <utility>
 
-class Cursor;
+
 class Line;
-class LinePos;
+
+class LinePos {
+    const Line *line;
+
+public:
+    int pos;
+
+    LinePos(const Line *line, int pos);
+    LinePos(const LinePos &line_pos);
+    
+    LinePos &operator=(const LinePos &other);
+
+    void move_r();
+
+    void move_l();
+
+    inline int operator()() const {
+        return pos;
+    }
+};
+
+struct Cursor {
+    LinePos pos;
+    LinePos anchor;
+
+    bool anchor_fixed = false;
+
+    Cursor(LinePos pos);
+
+    void fix_anchor();
+    void free_anchor();
+
+    void move_r();
+
+    void move_l();
+
+    void anchor_magnet();
+
+    bool is_selection();
+
+};
 
 /*
 [some_sting ]
@@ -14,7 +54,7 @@ class LinePos;
 */
 
 class Line {
-    std::vector<char>   data;
+    std::vector<char> data;
     Cursor cursor;
 
 public:
@@ -25,95 +65,105 @@ public:
     void insert_char(char c);
     void put_char(char c);
 
-    void delete_char();
-    void backspace_char();
+    void erase_next();
+    void erase_prev();
 
     void cut(bool include_border = false);
 
+    void cursor_l() { cursor.move_l(); }
+    void cursor_r() { cursor.move_r(); }
+
     void fix_anchors();
     void free_anchors();
-};
 
+    inline const char *c_str() const { return &data[0]; }
+    inline char *c_str() { return &data[0]; }
 
-class LinePos {
-    const Line *line;
+    inline void dump(char endline = '\n') const {
+        int pos = cursor.pos();
+        int anc = cursor.anchor();
 
-public:
-    int pos;
-
-    LinePos(const Line *line, int pos) :
-    line(line),
-    pos(pos)
-    {}
-
-    LinePos &operator=(const LinePos &other) {
-        line = other.line;
-        pos = other.pos;
-    }
-
-    void move_r() {
-        ++pos;
-        pos = pos >= line->len() ? line->len() - 1 : pos;
-    }
-
-    void move_l() {
-        --pos;
-        pos = pos < 0 ? 0 : pos;
-    }
-
-    int operator()() const {
-        return pos;
-    }
-};
-
-
-struct Cursor {
-    LinePos pos;
-    LinePos anchor;
-
-    bool anchor_fixed = false;
-
-
-    Cursor(LinePos pos) :
-    pos(pos),
-    anchor(pos)
-    {}
-
-    void fix_anchor()  { anchor_fixed = true; }
-    void free_anchor() { anchor_fixed = false; }
-
-    void move_r() {
-        pos.move_r();
-        if (!anchor_fixed) {
-            anchor = pos;
-        }
-    }
-
-    void move_l() {
-        pos.move_l();
-        if (!anchor_fixed) {
-            anchor = pos;
-        }
-    }
-
-    void anchor_magnet() {
-        if (pos() > anchor()) {
-            pos = anchor;
+        if (anc == pos) {
+            printf("[");
+            for (int i = 0; i < pos; ++i) {
+                printf("%c", data[i]);
+            }
+            printf("|");
+            for (int i = pos; i < len(); ++i) {
+                printf("%c", data[i]);
+            }
+            printf("]%c", endline);
         } else {
+            int mn = std::min(pos, anc);
+            int mx = std::max(pos, anc);
+
+            printf("[");
+            for (int i = 0; i < mn; ++i) {
+                printf("%c", data[i]);
+            }
+            printf("%c", mn == anc ? '{' : '|');
+            for (int i = mn; i < mx; ++i) {
+                printf("%c", data[i]);
+            }
+            printf("%c", mn == anc ? '|' : '}');
+            for (int i = mx; i < len(); ++i) {
+                printf("%c", data[i]);
+            }
+            printf("]%c", endline);
+        }
+    }
+};
+
+Cursor::Cursor(LinePos pos) :
+pos(pos),
+anchor(pos)
+{}
+
+void Cursor::fix_anchor()  { anchor_fixed = true; }
+void Cursor::free_anchor() { anchor_fixed = false; }
+
+void Cursor::move_r() {
+    if (is_selection()) {
+        if (pos() > anchor()) {
             anchor = pos;
+            return;
+        } else {
+            
         }
     }
 
-    bool is_selection() {
-        return pos() != anchor();
+    pos.move_r();
+    if (!anchor_fixed) {
+        anchor = pos;
     }
+}
 
-};
+void Cursor::move_l() {
+    pos.move_l();
+    if (!anchor_fixed) {
+        anchor = pos;
+    }
+}
 
-Line::Line(size_t to_reserve = 10) :
+void Cursor::anchor_magnet() {
+    if (pos() > anchor()) {
+        pos = anchor;
+    } else {
+        anchor = pos;
+    }
+}
+
+bool Cursor::is_selection() {
+    return pos() != anchor();
+}
+
+
+Line::Line(size_t to_reserve) :
 data(),
 cursor(LinePos(this, 0))
-{}
+{
+    data.reserve(to_reserve);
+}
 
 int Line::len() const { return (int) data.size(); }
 
@@ -124,38 +174,49 @@ void Line::cut(bool include_border) {
         std::swap(from, to);
     }
 
-    for (int w = from, r = to + include_border; r < data.size(); ++w, ++r) {
+    for (int w = from, r = to + include_border; r < len(); ++w, ++r) {
         data[w] = data[r];
     }
-    data.resize(data.size() - (from - to + include_border));
+    data.resize(data.size() - (to - from + include_border));
 
     cursor.anchor_magnet();
 }
 
 void Line::insert_char(char c) {
     cut();
+    printf("ins on pos %d\n", cursor.pos());
     data.insert(data.begin() + cursor.pos(), c);
+
+    if (cursor.pos() == len() - 1) {
+        data.push_back('\0');
+    }
+
     cursor.move_r();
 }
 
 void Line::put_char(char c) {
+    cursor.move_l();
     cut(true);
     data.insert(data.begin() + cursor.pos(), c);
-    cursor.move_r();
+    // cursor.move_r();
 }
 
-void Line::delete_char() {
-    cut(true);
-}
-
-void Line::backspace_char() {
-    if (!cursor.pos()) return;
-
+void Line::erase_next() {
     if (cursor.is_selection()) {
         cut();
     } else {
+        if (cursor.pos() == len() - 1) return;
+        cut(true);
+    }
+}
+
+void Line::erase_prev() {
+    if (cursor.is_selection()) {
+        cut();
+    } else {
+        if (!cursor.pos()) return;
         cursor.move_l();
-        delete_char();
+        erase_next();
     }
 }
 
@@ -165,4 +226,31 @@ void Line::fix_anchors() {
 
 void Line::free_anchors() {
     cursor.free_anchor();
+}
+
+LinePos::LinePos(const Line *line, int pos) :
+line(line),
+pos(pos)
+{}
+
+LinePos::LinePos(const LinePos &other) :
+line(other.line),
+pos(other.pos)
+{}
+
+LinePos &LinePos::operator=(const LinePos &other) {
+    line = other.line;
+    pos = other.pos;
+
+    return *this;
+}
+
+void LinePos::move_r() {
+    ++pos;
+    pos = pos >= line->len() ? line->len() - 1 : pos;
+}
+
+void LinePos::move_l() {
+    --pos;
+    pos = pos < 0 ? 0 : pos;
 }
